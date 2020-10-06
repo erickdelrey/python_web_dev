d93e12d54739d7ca0ca3bceeb605ff0ee7bee0bd
ErickDelRey <erick.del.rey5@gmail.com>
2020-10-06 22:16:43 +0800

    First commit

---
+++ server.py
@@ -0,0 +1,36 @@
+import csv
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return render_template('thankyou.html')
+        except Exception as e:
+            print(f'error: {e}')
+            return 'did not save to database'
+    return render_template('index.html')
+
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
